a7357068657c1edd669489493fb9842191347cd6
aenriq <aenriq29@asu.edu>
2024-06-05 09:32:52 -0700

    update ibl

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,8 +1,28 @@
+local highlight = {
+  "CursorColumn",
+  "Whitespace",
+}
+
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {},
+  opts = {
+},
   config = function()
-    require("ibl").setup()
+    vim.api.nvim_set_hl(0, "Green", { fg = "#5ccc96" })
+    require("ibl").setup({
+      indent = { highlight = highlight, char = " " },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+      },
+      scope = {
+        enabled = true,
+        char = "▏",
+        show_start = true,
+        show_end = true,
+        show_exact_scope = true,
+      },
+    })
   end
 }
